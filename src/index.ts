71a2af8cd98116aa96b6fc00465e20ab561d12cd
bramkaashoek <b.kaashoek@labdigital.nl>
2021-02-10 10:09:05 +0100

    add reexport of platform sdk

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { getApiRoot, getApiRootForCustomer } from './client';
 
 export { getApiRoot, getApiRootForCustomer };
+
+// reexport everything, so project always uses the same package version of platform sdk
+export * from '@commercetools/platform-sdk';
