5c682db9da968bd0461a54b7a570f68e0c8aac1c
bramkaashoek <b.kaashoek@labdigital.nl>
2021-02-11 16:44:28 +0100

    no longer reexport everything from platform sdk but have peer dependency instead

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import { getApiRoot, getApiRootForCustomer } from './client';
 
 export { getApiRoot, getApiRootForCustomer };
-
-// reexport everything, so project always uses the same package version of platform sdk
-export * from '@commercetools/platform-sdk';
